0ef0bcc80a6a1b946f44cb375600c1af9c56f60f
Blessed Sibanda <blasp.net@gmail.com>
2023-05-17 09:10:34 +0200

    Create Identity database and seed data

---
+++ SportsStore/Models/AppIdentityDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace SportsStore.Models;
+
+public class AppIdentityDbContext : IdentityDbContext<IdentityUser>
+{
+  public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options)
+    : base(options) { }
+}
